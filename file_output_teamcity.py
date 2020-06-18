a9f36976f602f4cff0a00bfb0523b6835e39eeba
Pro <ahtyrka@gmail.com>
2020-06-19 02:11:08 +0300

    updated json parsing and output

---
+++ file_output_teamcity.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+import json
+
+print("listing of href for non-archived(active) jobs:")
+
+with open('sample.json', mode='r') as f:
+    json_data = json.load(f)
+
+print("listing of href for non-archived(active) jobs:\n==============\n")
+
+count = 0
+for json_dict in json_data['project']:
+
+    if not 'archived' in json_dict.keys():
+         #print("value = ",json_dict['href'],json_dict['archived'])
+         print(json_dict['id'], "value = ",json_dict['href'])
+         count += 1
+
+
+print("\n==============\n",count,"records")
--- output_teamcity_json.py
+++ output_teamcity_json.py
@@ -13,8 +13,18 @@ from requests import get
 get_data = get('http://localhost:8080/app/rest/projects', auth=(TC_USERNAME, TC_PASSWORD), headers=headers)
 
 
-#print(url.content)
 
-formatted_json = json.dumps(json.loads(get_data.content), indent=4)
+#formatted_json = json.dumps(json.loads(get_data.content), indent=4)
+formatted_json = json.loads(get_data.content)
 
-print(formatted_json)
+print("listing of href for non-archived(active) jobs:\n==============\n")
+
+count = 0
+for json_dict in formatted_json['project']:
+
+    if not 'archived' in json_dict.keys():
+         print(json_dict['id'], "value = ",json_dict['href'])
+         count += 1
+
+
+print("\n==============\n",count,"records")
