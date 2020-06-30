ed0d47053c85f9a31a6d4cddc92ffa92f845ae8f
Pro <ahtyrka@gmail.com>
2020-06-30 18:35:29 +0300

    commit some intendations, update output

--- output_teamcity_json.py
+++ output_teamcity_json.py
@@ -23,28 +23,28 @@ count = 0
 for json_dict in formatted_json['project']:
 
     if not 'archived' in json_dict.keys():
-         # output href
-         #print(json_dict['id'], "value = ",json_dict['href'])
+        # output href
+        #print(json_dict['id'], "value = ",json_dict['href'])
 
-         project_id = json_dict['href'].split('id:')[1]
-         #print ("id = ", project_id)
+        project_id = json_dict['href'].split('id:')[1]
+        #print ("id = ", project_id)
 
-         activebranch_url = "http://localhost:8080/app/rest/projects/"+project_id+"/branches?locator=policy:ACTIVE_VCS_BRANCHES"
-         print(activebranch_url)
+        activebranch_url = "http://localhost:8080/app/rest/projects/"+project_id+"/branches?locator=policy:ACTIVE_VCS_BRANCHES"
+        print(activebranch_url)
 
-         get_activebranch = get(activebranch_url, auth=(TC_USERNAME, TC_PASSWORD), headers=headers)
+        get_activebranch = get(activebranch_url, auth=(TC_USERNAME, TC_PASSWORD), headers=headers)
 
-         formatted_abranches = json.loads(get_activebranch.content)
-         if formatted_abranches['count'] != 0:
-             # output json
-             # print('result = ',formatted_abranches)
-             for abranches_dict in formatted_abranches['branch']:
-                 print("project = ",project_id,"active branch = ",abranches_dict['name'])
-             print("\n")
-         else:
-             print("\n")
+        formatted_abranches = json.loads(get_activebranch.content)
+        if formatted_abranches['count'] != 0:
+            # output json
+            # print('result = ',formatted_abranches)
+            for abranches_dict in formatted_abranches['branch']:
+                print("Project = ",project_id,"; Active branch = ",abranches_dict['name'])
+            print("\n")
+        else:
+            print("\n")
 
-         count += 1
+        count += 1
 
 
 print("\n==============\n",count,"records")
