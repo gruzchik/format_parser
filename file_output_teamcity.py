867bf1c795a6a96793a3dc0fc6e82887e56a96fa
Pro <ahtyrka@gmail.com>
2020-07-01 15:44:02 +0300

    update active branches for file_output_teamcity.py

--- file_output_teamcity.py
+++ file_output_teamcity.py
@@ -9,6 +9,8 @@ with open('sample.json', mode='r') as f:
 print("listing of href for non-archived(active) jobs:\n==============\n")
 
 count = 0
+connection_status = 0
+
 for json_dict in json_data['project']:
 
     if not 'archived' in json_dict.keys():
@@ -20,6 +22,20 @@ for json_dict in json_data['project']:
 
          activebranch_url = "http://localhost:8080/app/rest/projects/"+project_id+"/branches?locator=policy:ACTIVE_VCS_BRANCHES"
          print(activebranch_url)
+       
+         if connection_status == 1:
+             get_activebranch = get(activebranch_url, auth=(TC_USERNAME, TC_PASSWORD), headers=headers)
+
+             formatted_abranches = json.loads(get_activebranch.content)
+             if formatted_abranches['count'] != 0:
+                 # output json
+                 # print('result = ',formatted_abranches)
+                 for abranches_dict in formatted_abranches['branch']:
+                     print("project = ",project_id,"active branch = ",abranches_dict['name'])
+                 print("\n")
+             else:
+                 print("\n")
+
 
          count += 1
 
