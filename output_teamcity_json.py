143b267bb0867fbf0b399335a9b0a14759d5ef2b
Igor Pogorelko <ahtyrka@gmail.com>
2020-07-06 15:51:38 +0300

    Update output_teamcity_json.py

--- output_teamcity_json.py
+++ output_teamcity_json.py
@@ -10,7 +10,7 @@ headers = {
 }
 
 from requests import get
-get_data = get('http://localhost:8080/app/rest/projects', auth=(TC_USERNAME, TC_PASSWORD), headers=headers)
+get_data = get(TC_URL+"/app/rest/projects", auth=(TC_USERNAME, TC_PASSWORD), headers=headers)
 
 
 
@@ -29,7 +29,7 @@ for json_dict in formatted_json['project']:
             project_id = json_dict['href'].split('id:')[1]
             #print ("id = ", project_id)
 
-            activebranch_url = "http://localhost:8080/app/rest/projects/"+project_id+"/branches?locator=policy:ACTIVE_VCS_BRANCHES"
+            activebranch_url = TC_URL+"/app/rest/projects/"+project_id+"/branches?locator=policy:ACTIVE_VCS_BRANCHES"
 
             get_activebranch = get(activebranch_url, auth=(TC_USERNAME, TC_PASSWORD), headers=headers)
 
