019edfea93a757a2bf419240fd1bbb3c840198db
Pro <ahtyrka@gmail.com>
2020-07-17 13:55:05 +0300

    creating settings,py in rundocker.sh

--- rundocker.sh
+++ rundocker.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+cat <<EOF >> settings.py
+TC_USERNAME=$user_variable
+TC_PASSWORD=$pass_variable
+TC_URL=$teamcity_url
+EOF
+
 docker build -t pp .
 docker run --rm -v $(pwd):/app pp python /app/output_teamcity_json.py
